a4232db06380f70ab80f593549a7dffac395c718
许友壤 <772884291@qq.com>
2019-07-23 21:08:15 +0800

    分页、条件查询基类增加排序

--- src/main/java/cn/xyr/ssm/common/model/condition/BaseCondition.java
+++ src/main/java/cn/xyr/ssm/common/model/condition/BaseCondition.java
@@ -1,11 +1,12 @@
 package cn.xyr.ssm.common.model.condition;
 
 
+import com.github.miemiedev.mybatis.paginator.domain.Order;
 import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
 
 /**
- * BaseCondition
- *
+ * 分页条件查询基类
+ * 与物理分页插件配合使用
  */
 public class BaseCondition {
 
@@ -18,49 +19,60 @@ public class BaseCondition {
      * The Page limit. 若无赋值默认为每页10条数据
      */
     private Integer pageLimit = 10;
+    /**
+     * 排序方式
+     * 升序ASC，降序DESC
+     */
+    private String sort = null;
+    /**
+     * 排序字段
+     * 需和数据库字段相同
+     */
+    private String sortParam = null;
 
     /**
      * Gets page bounds.
+     * 获取分页器
      *
      * @return the page bounds
      */
     public PageBounds getPageBounds() {
-        return new PageBounds(pageNo, pageLimit, true);
+        PageBounds pageBounds = new PageBounds(pageNo, pageLimit, true);
+        if (sortParam != null && sort != null) {
+            pageBounds.setOrders(Order.formString(sortParam + "." + sort));
+        }
+        return pageBounds;
     }
 
-    /**
-     * Sets page no.
-     *
-     * @param pageNo the page no
-     */
     public void setPageNo(Integer pageNo) {
         this.pageNo = pageNo;
     }
 
-    /**
-     * Gets page no.
-     *
-     * @return the page no
-     */
     public Integer getPageNo() {
         return pageNo;
     }
 
-    /**
-     * Gets page limit.
-     *
-     * @return the page limit
-     */
     public Integer getPageLimit() {
         return pageLimit;
     }
 
-    /**
-     * Sets page limit.
-     *
-     * @param pageLimit the page limit
-     */
     public void setPageLimit(Integer pageLimit) {
         this.pageLimit = pageLimit;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getSortParam() {
+        return sortParam;
+    }
+
+    public void setSortParam(String sortParam) {
+        this.sortParam = sortParam;
     }
 }
